9c710d873787fcb9febac94df01fbbb95ef425da
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-03-21 00:31:49 +0200

    Added page for user, made site bootstrap friendly

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
   resources :events
   resources :years
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  resources :users, only: [:show]
+
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'logout', to: 'devise/sessions#destroy'
   end
+
+
 
 
   root to: "events#index"
