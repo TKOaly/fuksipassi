1e6e280ab5763e1bf8e79be9f30d6fc6a2271a89
Eetu Mattila <eetu_mattila@hotmail.com>
2017-03-20 15:30:23 +0200

    Email enviroments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   rolify
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :timeoutable
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:fuksi) if self.roles.blank?
+  end
 end
