dff5d17af19435729c4963743845812afe91d289
Eetu Mattila <eetu_mattila@hotmail.com>
2017-03-28 16:48:45 +0300

    Authorizations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  load_and_authorize_resource
+  check_authorization :unless => :devise_controller?
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   before_action :authenticate_user!
@@ -11,5 +14,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 
-
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, alert: exception.message }
+      format.js   { head :forbidden, content_type: 'text/html' }
+    end
+  end
 end
--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,4 +1,5 @@
 class ParticipationsController < ApplicationController
+  load_and_authorize_resource
   before_action :set_participation, only: [:show, :edit, :update, :destroy]
 
   # GET /participations
