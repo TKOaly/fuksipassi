21d5545a6d11aa3437c3f90ed059f38e65a315e4
Eetu Mattila <eetu_mattila@hotmail.com>
2017-03-28 20:25:10 +0300

    Index authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   check_authorization :unless => :devise_controller?
+  load_and_authorize_resource
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
