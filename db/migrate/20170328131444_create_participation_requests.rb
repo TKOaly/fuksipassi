5b4cb4e8de1809f47688cc9478697967fc06fab7
Eetu Mattila <eetu_mattila@hotmail.com>
2017-04-01 17:15:13 +0300

    Fucked up the db

--- db/migrate/20170328131444_create_participation_requests.rb
+++ db/migrate/20170328131444_create_participation_requests.rb
@@ -1,10 +1,10 @@
 class CreateParticipationRequests < ActiveRecord::Migration[5.0]
   def change
     create_table :participation_requests do |t|
-      t.references :acceptor_id, foreign_key: true
-      t.references :participant_id, foreign_key: true
+      t.references :acceptor, foreign_key: true
+      t.references :participant, foreign_key: true
       t.string :description
-      t.references :participation_id, foreign_key: true
+      t.references :participation, foreign_key: true
       t.boolean :accepted
 
       t.timestamps
