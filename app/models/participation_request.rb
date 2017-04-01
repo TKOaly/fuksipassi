58918fc5c356fd118c454ba5df3231278d2ac51a
Eetu Mattila <eetu_mattila@hotmail.com>
2017-04-01 17:55:24 +0300

    Fucked up db

--- app/models/participation_request.rb
+++ app/models/participation_request.rb
@@ -1,8 +1,9 @@
 class ParticipationRequest < ApplicationRecord
-  belongs_to :acceptor, :class_name => 'User', :foreign_key => 'acceptor_id'
-  belongs_to :participant, :class_name => 'User', :foreign_key => 'participant_id'
+  belongs_to :acceptor, :class_name => 'User'
+  belongs_to :participant, :class_name => 'User'
   belongs_to :participation
   has_one :event, through: :participation
+
   # has_one :points, through: :participation
 
   scope :unconfirmed, -> {where('ACCEPTOR_ID IS NULL')}
--- db/migrate/20170328131444_create_participation_requests.rb
+++ db/migrate/20170328131444_create_participation_requests.rb
@@ -1,10 +1,10 @@
 class CreateParticipationRequests < ActiveRecord::Migration[5.0]
   def change
     create_table :participation_requests do |t|
-      t.references :acceptor, foreign_key: true
-      t.references :participant, foreign_key: true
+      t.references :acceptor
+      t.references :participant
       t.string :description
-      t.references :participation, foreign_key: true
+      t.references :participation
       t.boolean :accepted
 
       t.timestamps
