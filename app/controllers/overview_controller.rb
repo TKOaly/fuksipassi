cb49136e57868426a83e02385283e94944355484
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-04-18 17:08:00 +0300

    Unconfirmed can be selected

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -2,6 +2,6 @@ class OverviewController < ApplicationController
 
   def index
     @participations = Participation.all
-    @unconfirmed_requests = ParticipationRequest.unconfirmed.map { |p| p.participation }.uniq.map { |event| event.participation_requests.unconfirmed }
+    @unconfirmed_requests = ParticipationRequest.participations_with_unconfirmed
   end
 end
