728d3edfc4b68df2ec9cbf14c28b43f375379ee5
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-04-25 17:40:54 +0300

    Fixed event links

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -77,6 +77,6 @@ class EventsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def event_params
-    params.require(:event).permit(:name, :date, :url, :points)
+    params.require(:event).permit(:name, :date, :event_link, :points)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421143604) do
+ActiveRecord::Schema.define(version: 20170425143042) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name",       null: false
     t.date     "date"
-    t.string   "url"
+    t.string   "event_link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "year_id"
