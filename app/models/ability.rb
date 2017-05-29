49e2407565170826c1996716bdbcb8d87d81b2d6
Eetu Mattila <eetu_mattila@hotmail.com>
2017-05-29 19:17:59 +0300

    Aloitettu authorisaatioden korjaus

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,14 +5,13 @@ class Ability
     user ||= User.new # guest user (not logged in)
 
     if user.has_role? :admin
-      can :manage, :all
+       can :manage, :all
     end
 
     if user.has_role? :fuksi
       can :read, Event
-      can :read, Participation
-      can :read, User
-      can :create, Participation
+      can [:read, :create, :update, :destroy], [Participation, Note]
+      can [:read, :create, :update, :destroy], User, id: user.id
     end
   end
 end
