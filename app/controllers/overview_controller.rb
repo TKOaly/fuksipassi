fc056cb3cacc6da828886d4c8d0341d74b1750e9
Eetu Mattila <eetu_mattila@hotmail.com>
2017-08-26 16:11:30 +0300

    Trying to fix things

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -14,7 +14,7 @@ class OverviewController < ApplicationController
     else
       @unconfirmed_requests = all_unconfirmed_requests
     end
-    @unconfirmed_requests = @unconfirmed_requests.sort_by {|p| [p.event, p.participation, p.participant] }
+    @unconfirmed_requests = @unconfirmed_requests.sort_by {|p| [p.event, p.participation] }
     @events = all_unconfirmed_requests.collect(&:event).uniq.sort_by(&:name)
     @users = all_unconfirmed_requests.collect(&:participant).uniq.sort_by {|u| [u.last_name, u.first_name] }
   end
