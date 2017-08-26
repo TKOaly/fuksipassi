04286b6f06000b070c374fa644706e9e4170d483
Eetu Mattila <eetu_mattila@hotmail.com>
2017-08-26 15:40:46 +0300

    Small edit to overview ordering

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -6,14 +6,15 @@ class OverviewController < ApplicationController
   def index
     all_unconfirmed_requests = ParticipationRequest.unconfirmed
     if params[:event]
-      @unconfirmed_requests = all_unconfirmed_requests.joins(:participation).where(participations: { event_id: params[:event] }).sort_by {|p| [p.event, p.participation] }
+      @unconfirmed_requests = all_unconfirmed_requests.joins(:participation).where(participations: { event_id: params[:event] })
       @selected_event = Event.find_by_id params[:event]
     elsif params[:user]
-      @unconfirmed_requests = all_unconfirmed_requests.where(participant_id: params[:user]).sort_by {|p| [p.event, p.participation] }
+      @unconfirmed_requests = all_unconfirmed_requests.where(participant_id: params[:user])
       @selected_user = User.find_by_id params[:user]
     else
-      @unconfirmed_requests = all_unconfirmed_requests.sort_by {|p| [p.event, p.participation] }
+      @unconfirmed_requests = all_unconfirmed_requests
     end
+    @unconfirmed_requests = @unconfirmed_requests.sort_by {|p| [p.event, p.participation, p.participant] }
     @events = all_unconfirmed_requests.collect(&:event).uniq.sort_by(&:name)
     @users = all_unconfirmed_requests.collect(&:participant).uniq.sort_by {|u| [u.last_name, u.first_name] }
   end
