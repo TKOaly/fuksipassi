f088f789e0e4deabd876f42a9d922d17f76c59d3
Eetu Mattila <eetu_mattila@hotmail.com>
2017-08-26 16:59:09 +0300

    Removal

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -14,7 +14,7 @@ class OverviewController < ApplicationController
     else
       @unconfirmed_requests = all_unconfirmed_requests
     end
-    @unconfirmed_requests = @unconfirmed_requests.compact.sort_by {|p| [p.event, p.participation, p.participant.first_name, p.participant.last_name]}
+    @unconfirmed_requests = @unconfirmed_requests.sort_by {|p| [p.event, p.participation, p.participant.first_name, p.participant.last_name]}
     @events = all_unconfirmed_requests.collect(&:event).compact.uniq.sort_by(&:name)
     @users = all_unconfirmed_requests.collect(&:participant).compact.uniq.sort_by {|u| [u.first_name, u.last_name]}
   end
