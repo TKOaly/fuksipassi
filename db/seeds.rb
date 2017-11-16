eb9e893979d0b86d1870d98547b02bff23e089e7
zHarrowed <eetu_mattila@hotmail.com>
2017-11-17 01:24:56 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(username: 'julkku', first_name: 'Julius', last_name: 'Uusinarkaus', irc_nick: 'julkku', email: 'julius@uusinarkaus.com', password: ENV['PASS'], password_confirmation: ENV['PASS'], confirmed_at: Date.today)
-User.create(username: 'Harrowed', first_name: 'Eetu', last_name: 'Mattila', irc_nick: 'Harrowed', email: 'eetu.mattila@cs.helsinki.fi', password: ENV['PASS'], password_confirmation: ENV['PASS'], confirmed_at: Date.today)
+User.create(username: ENV['USERNAME'], first_name: ENV['FIRSTNAME'], last_name: ENV['LASTNAME'], irc_nick: ENV['IRCNICK'], email: ENV['EMAIL'], password: ENV['PASS'], password_confirmation: ENV['PASS'], confirmed_at: Date.today)
 User.first.add_role 'admin'
 User.second.add_role 'admin'
 Language.create(name: 'English', en_name: 'English', code: 'en')
